3b9ca2e2fb7fbfc53d270e4e7e6465923f57ae60
gloomydumber <chromacrow@naver.com>
2024-09-03 00:15:01 +0900

    `refactor` : implement WGT using OpenZeppelin's ERC20Votes

--- src/WGT.sol
+++ src/WGT.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WeatteryGovernanceToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("Weattery Governance Token", "WGT") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+contract WeatteryGovernanceToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+    constructor()
+        ERC20("Weattery Governance Token", "WGT")
+        ERC20Permit("Weattery Governance Token")
+        Ownable(msg.sender)
+    {}
+
+    // The functions below are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function burn(address from, uint256 amount) public onlyOwner {
-        _burn(from, amount);
+    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
